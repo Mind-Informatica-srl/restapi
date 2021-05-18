5f90805c3f7345db1cd203a65e0ea69f152945c9
simonerocchi <s.rocchi@mind-informatica.com>
2021-05-18 12:13:45 +0200

    Update model.go
    base delegate

--- pkg/models/model.go
+++ pkg/models/model.go
@@ -1,12 +1,57 @@
 // PAckage models provide some utils stuff to manage models in restapi server
 package models
 
+import (
+	"net/http"
+
+	"gorm.io/gorm"
+)
+
 // PKModel expose functions needed to manage the primary key in a model
 type PKModel interface {
-	// ExtractPK return the pk of the model
-	ExtractPK() interface{}
 	// SetPK set the pk for the model
 	SetPK(pk interface{}) error
 	// VerifyPK check the pk value
 	VerifyPK(pk interface{}) (bool, error)
+}
+
+// BaseDelegate implements the DBDelegate interfaces for a model implementig the PKModel interface
+type BaseDelegate struct {
+	DBProvider    func() *gorm.DB
+	ObjectCreator func() PKModel
+	ListCreator   func() []PKModel
+	PKExtractor   func(r *http.Request) (interface{}, error)
+}
+
+func (d BaseDelegate) ExtractPK(r *http.Request) (interface{}, error) {
+	return d.PKExtractor(r)
+}
+
+func (d BaseDelegate) ProvideDB() *gorm.DB {
+	return d.DBProvider()
+}
+
+func (d BaseDelegate) AssignPK(element PKModel, pk interface{}) error {
+	return element.SetPK(pk)
+}
+
+func (d BaseDelegate) CreateObject() interface{} {
+	return d.ObjectCreator()
+}
+
+func (d BaseDelegate) CreateList() interface{} {
+	return d.ListCreator()
+}
+
+func (d BaseDelegate) VerifyPK(element PKModel, pk interface{}) (bool, error) {
+	return element.VerifyPK(pk)
+}
+
+func NewBaseDelegate(dbProvider func() *gorm.DB, objectCreator func() PKModel, listCreator func() []PKModel, pkExtractor func(r *http.Request) (interface{}, error)) BaseDelegate {
+	return BaseDelegate{
+		DBProvider:    dbProvider,
+		ObjectCreator: objectCreator,
+		ListCreator:   listCreator,
+		PKExtractor:   pkExtractor,
+	}
 }
