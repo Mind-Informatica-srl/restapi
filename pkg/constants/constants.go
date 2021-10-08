9d33f4736839917c435fefeed63c76b886604d5e
lorenzoB07 <82948587+lorenzoB07@users.noreply.github.com>
2021-10-08 12:44:32 +0200

    Aggiunta constant DateFormatStringYYYYMMDDTHHMMSSZ

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,6 +2,8 @@
 package constants
 
 const (
+	DateFormatStringYYYYMMDDTHHMMSSZ = "2006-01-02T15:04:05Z"
+
 	DateFormatStringYYYYMMDDTHHMMSS = "2006-01-02T15:04:05"
 
 	DateFormatStringYYYYMMDD = "2006-01-02"
