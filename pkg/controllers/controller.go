69d8f934ca77f576e77ec36b9d7ac5cdf882a399
simonerocchi <s.rocchi@mind-informatica.com>
2022-04-23 13:55:24 +0200

    Update controller.go

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -7,6 +7,12 @@ type Controller struct {
 	Actions []actions.AbstractAction
 }
 
+// ModifyActions modifica le azioni e restituisce il controller
+func (c Controller) ModifyActions(modifier func([]actions.AbstractAction) []actions.AbstractAction) Controller {
+	c.Actions = modifier(c.Actions)
+	return c
+}
+
 func (c *Controller) AddAction(action actions.AbstractAction) {
 	c.Actions = append(c.Actions, action)
 }
