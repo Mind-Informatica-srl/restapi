eccbf5a565fcb053b1b01c3237ba59d8eb5855a1
simonerocchi <s.rocchi@mind-informatica.com>
2024-10-19 19:58:27 +0200

    Update pager.go
    esposto actions.ExtractPage

--- pkg/actions/pager.go
+++ pkg/actions/pager.go
@@ -25,18 +25,7 @@ func (p Pager) MarshalJSON() ([]byte, error) {
 
 // Paginate restituisce scope per impaginare i risultati di una query
 func Paginate(r *http.Request) (paginateScope func(db *gorm.DB) *gorm.DB, page int, pageSize int) {
-	var pageString, pageSizeString string
-	if params, ok := r.URL.Query()["page"]; ok {
-		pageString = params[0]
-	}
-	if params, ok := r.URL.Query()["pageSize"]; ok {
-		pageSizeString = params[0]
-	}
-	if pageString != "" && pageSizeString != "" {
-		pageSize, _ = strconv.Atoi(pageSizeString)
-		page, _ = strconv.Atoi(pageString)
-		page--
-	}
+	page, pageSize = ExtractPage(r)
 	paginateScope = PaginateScope(page, pageSize)
 	return
 }
@@ -49,4 +38,20 @@ func PaginateScope(page int, pageSize int) func(db *gorm.DB) *gorm.DB {
 		}
 		return db
 	}
+}
+
+func ExtractPage(r *http.Request) (page int, pageSize int) {
+	var pageString, pageSizeString string
+	if params, ok := r.URL.Query()["page"]; ok {
+		pageString = params[0]
+	}
+	if params, ok := r.URL.Query()["pageSize"]; ok {
+		pageSizeString = params[0]
+	}
+	if pageString != "" && pageSizeString != "" {
+		pageSize, _ = strconv.Atoi(pageSizeString)
+		page, _ = strconv.Atoi(pageString)
+		page--
+	}
+	return
 }
