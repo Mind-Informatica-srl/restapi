bac2af50375e1759725bdf2c36ba867131b2e728
simonerocchi <s.rocchi@mind-informatica.com>
2025-01-19 20:26:23 +0100

    Update server.go

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -107,6 +107,12 @@ func requestCorsMiddleware() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
+			// Check if the request is a preflight request
+			if req.Method == http.MethodOptions && req.Header.Get("Access-Control-Request-Method") != "" {
+				// Handle preflight request
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
 			next.ServeHTTP(w, req)
 		})
 	}
